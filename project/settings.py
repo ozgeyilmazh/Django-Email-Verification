c16dfbdddd126c34fb26e7c3531cb66be4e7701b
Ozge <30619259+ozgeyilmazh@users.noreply.github.com>
2022-05-15 15:18:34 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -141,8 +141,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 #outllok açılcak
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ="smtp.outlook.com"
-EMAIL_HOST_USER ="ozgeyilmazh@outlook.com"
-EMAIL_HOST_PASSWORD = "1Ozgeyilmaz."
+EMAIL_HOST_USER =""
+EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "ozgeyilmazh@outlook.com"
